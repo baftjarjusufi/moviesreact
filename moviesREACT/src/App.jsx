5b561e3f03c50ba1e34270f6f3b58e94a0dbf62d
Baftjar Jusufi <baftjarjj@gmail.com>
2025-02-11 19:39:01 +0100

    simple styling

--- moviesREACT/src/App.jsx
+++ moviesREACT/src/App.jsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from 'react';
+
 const Card = ({title }) => {
+  const [count, setCount] = useState(0);
+
+  const [hasliked,setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasliked}`);
+  }, [hasliked]);
+
+
+
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>{title} - <br/>{count ? count : null}</h2>
+
+      <button onClick={() => setHasLiked(!hasliked)}>
+        {hasliked ? '❤️' : '🤍'}
+      </button>
     </div>
   )
 
 }
 
 
-  const App = () => {
+const App = () => {
+
+
+
   return (
     <div className="card-container">
 
@@ -20,7 +40,7 @@ const Card = ({title }) => {
 
   )
 
-  }
+}
 
 
 
