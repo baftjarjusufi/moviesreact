65fd71b954cfad12cec2ac9a3b04e57ac1ace5ec
Baftjar Jusufi <baftjarjj@gmail.com>
2025-02-11 18:16:58 +0100

    test

---
+++ moviesREACT/src/App.jsx
@@ -0,0 +1,27 @@
+const Card = ({title }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+    </div>
+  )
+
+}
+
+
+  const App = () => {
+  return (
+    <div>
+    <h2>Functional Arrow Component</h2>
+
+    <Card  title ="Star Wars"/>
+    <Card  title ="Avatar "/>
+    <Card  title ="The lion King."/>
+    </div>
+
+  )
+
+  }
+
+
+
+export default App
